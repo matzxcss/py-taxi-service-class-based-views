fe39a79204561fcd275e7260e58bcd96f8d6f473
matzxcss <matheuslacerda01@proton.me>
2026-04-08 12:35:49 -0300

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 
-from .views import index
+from taxi.views import (
+    CarDetailView,
+    CarListView,
+    DriverDetailView,
+    DriverListView,
+    ManufacturerListView,
+    index,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "manufacturer/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list",
+    ),
+    path("car/", CarListView.as_view(), name="car-list"),
+    path("car/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("driver/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("driver/", DriverListView.as_view(), name="driver-list"),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-from taxi.models import Driver, Car, Manufacturer
+from taxi.models import Car, Driver, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
 
     context = {
         "num_drivers": Driver.objects.count(),
@@ -13,3 +13,38 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    queryset = (
+        Car.objects.select_related("manufacturer").all().order_by("model")
+    )
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+    queryset = (
+        Car.objects.prefetch_related("drivers")
+        .select_related("manufacturer")
+        .all()
+    )
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    queryset = Driver.objects.all().order_by("username")
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    queryset = Driver.objects.prefetch_related("cars").all()
